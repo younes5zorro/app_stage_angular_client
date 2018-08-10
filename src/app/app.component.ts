3d6a307eee519fe0850a6250c265b8f08a25778a
younes5zorro <younes5zorro@gmail.com>
2018-08-10 17:18:56 +0100

    9- add loading page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,22 +8,17 @@ import { Router } from '../../node_modules/@angular/router';
 })
 export class AppComponent {
   title = 'app';
-  router: Router;
   message = '';
   constructor(private _router: Router ) {
       _router.events.subscribe((url: any) => {
         if (url.url === '/form') {
           this.message = 'Profil investisseur';
         } else {
-          if (url.url === '/') {
-            this.message = 'Home';
+          if (url.url === '/dashboard') {
+            this.message = 'Transaction';
           } else {
-            if (url.url === '/dashboard') {
-              this.message = 'Composition';
-            } else {
-              this.message = '';
-            }
-          }
+           this.message = '';
+           }
         }
       } );
       // console.log(_router.url);
